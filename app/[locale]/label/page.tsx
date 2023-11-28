c2eb96707edfed5f4af201c0dda401241ebe5dcb
dobzacode <ckittel67@gmail.com>
2023-11-28 15:10:46 +0100

    label page done

--- app/[locale]/label/page.tsx
+++ app/[locale]/label/page.tsx
@@ -1,4 +1,8 @@
-import ImageSlider from 'components/ui/div/image-slider/image-slider';
+import ReleaseCard from 'components/label/release-card';
+import releases from 'components/label/release.json';
+import { H1 } from 'components/ui/text/h1';
+import P from 'components/ui/text/p';
+import { v4 } from 'uuid';
 
 export const metadata = {
   description: 'High-performance ecommerce store built with Next.js, Vercel, and Shopify.',
@@ -9,8 +13,35 @@ export const metadata = {
 
 export default async function HomePage() {
   return (
-    <>
-      <ImageSlider imageFolder="/asset/event/media/pulsar-lulu"></ImageSlider>
-    </>
+    <main className="relative flex min-h-screen flex-col items-center gap-sub-extra-large overflow-x-hidden px-small py-extra-large tablet:px-0">
+      <section className="relative flex flex-col ">
+        <H1
+          textType={'heading--extra-large'}
+          className="slideInFromTop relative z-10 -mb-large text-center text-black5 max-mobile-large:text-heading-large max-mobile-large:leading-heading-large tablet:mt-small"
+        >
+          LABEL
+        </H1>
+        <div className="slideInFromBottom flex h-fit w-fit max-w-[600px] flex-col justify-between gap-medium overflow-hidden rounded-small bg-black95 p-medium pt-[8rem] text-center text-black5 shadow-medium-light">
+          <P intent="white" textType={'body'} className="pr-small font-extralight">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris
+          </P>
+        </div>
+      </section>
+      <div className="flex max-w-[1100px] flex-wrap items-center justify-center gap-extra-large px-small">
+        {releases.map(({ link, pictureSrc, name }, index) => {
+          return (
+            <ReleaseCard
+              key={v4()}
+              pictureSrc={pictureSrc}
+              name={name}
+              link={link}
+              direction={index % 2 == 0 ? 'left' : 'right'}
+            ></ReleaseCard>
+          );
+        })}
+      </div>
+    </main>
   );
 }
---
+++ components/label/release-card.tsx
@@ -0,0 +1,48 @@
+import P from 'components/ui/text/p';
+import { cn } from 'lib/utils';
+import Image from 'next/image';
+
+export interface ReleaseCardProps {
+  direction: 'right' | 'left';
+  link: string;
+  pictureSrc: string;
+  name: string;
+}
+
+export default function ReleaseCard({
+  direction,
+  link,
+  pictureSrc,
+  name,
+  ...props
+}: ReleaseCardProps) {
+  return (
+    <section
+      className={cn(
+        'relative flex h-fit  w-fit justify-end  gap-medium   overflow-hidden  rounded-small bg-black95 text-black5  shadow-medium-light laptop:gap-sub-large',
+        direction === 'left'
+          ? 'slideInFromLeft transition-delay-1000'
+          : 'slideInFromRight transition-delay-1000'
+      )}
+      {...props}
+    >
+      <a
+        href={link}
+        className="absolute z-10 flex h-full w-full cursor-pointer items-center justify-center bg-black bg-opacity-0 opacity-0 duration-medium hover:bg-opacity-80 hover:opacity-100"
+      >
+        <P intent="white" textType={'heading--sub-large'} className="">
+          {name}
+        </P>
+      </a>
+      <div className={cn('relative  h-[40rem]   w-[40rem]  ')}>
+        <Image
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          fill
+          className="object-cover"
+          src={pictureSrc}
+          alt={`${name} artwork`}
+        ></Image>
+      </div>
+    </section>
+  );
+}
