01cd5c80c3613d9def33205e89e853ae3131f048
dobzacode <contact@corentinkittel.fr>
2024-09-02 18:24:47 +0200

    add safety check for image render in gallery

--- components/media/photo-video/media-portion.tsx
+++ components/media/photo-video/media-portion.tsx
@@ -2,13 +2,18 @@ import ImageSlider from 'components/ui/div/image-slider/image-slider';
 import { Event, Image } from '../../../sanity/lib/queries';
 import { urlForImage } from '../../../sanity/lib/utils';
 
+function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
+  return value !== null && value !== undefined;
+}
+
 export default async function MediaPortion({ event }: { event: Event }) {
+  console.log(event);
   const imagesWithUrl = event.imageGallery
     ? await Promise.all(
         event.imageGallery.map(async (image: Image) => {
+          if (!image.asset) return;
           image.url = await urlForImage(image).width(1920).height(1080).dpr(2).quality(100).url();
-
-          image.blurSrc = urlForImage(image).width(20).quality(20).url();
+          image.blurSrc = await urlForImage(image).width(20).quality(20).url();
           return image;
         })
       )
@@ -16,5 +21,7 @@ export default async function MediaPortion({ event }: { event: Event }) {
 
   if (!imagesWithUrl) return null;
 
-  return <ImageSlider images={imagesWithUrl}></ImageSlider>;
+  const imagesArr = imagesWithUrl.filter(notEmpty);
+
+  return <ImageSlider images={imagesArr}></ImageSlider>;
 }
